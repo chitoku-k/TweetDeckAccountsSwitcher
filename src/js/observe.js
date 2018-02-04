62e8cbaeda7bcb3e60c1e11805941dd71382ae44
Chitoku <chitoku-k@users.noreply.github.com>
2018-02-04 18:12:11 +0900

    Fix #5: Follow spec change of retweet modal (#7)

--- src/js/observe.js
+++ src/js/observe.js
@@ -7,22 +7,22 @@ export function observe() {
     const modal = $("#actions-modal");
     const observer = new MutationObserver(() => {
         const selector = modal.find("ul.js-account-selector");
-        let selected = selector.find(".acc-selected").data("id");
+        let selected = selector.find(".is-selected").data("id");
 
         selector.on("td-accounts-change", (e, { selectedAccounts }) => {
             // Cancel when the number of accounts <= 1
             if (selectedAccounts.length <= 1) {
                 // Assign the last selected account or undefined
                 const [ current ] = selectedAccounts;
-                selected = current ? current.account : null;
+                selected = current ? current.accountKey : null;
                 return;
             }
 
             // Gets the last selected account
-            const [ current ] = selectedAccounts.filter(x => selected !== x.account);
+            const [ current ] = selectedAccounts.filter(x => selected !== x.accountKey);
 
             // Click all the selected accounts except the last selected one
-            selector.find(".acc-selected").each((i, e) => e.dataset.id === current.account || e.click());
+            selector.find(".is-selected").each((i, e) => e.dataset.id === current.accountKey || e.click());
         });
     });
 
